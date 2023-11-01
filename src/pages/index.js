24b4c280c8a12da6c0a681bf0040657ec289ef0e
YamacGN <yamacgence@gmail.com>
2023-11-01 23:12:52 +0300

    Redirect update

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import  { Redirect } from 'react-router-dom'
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+import React from 'react'
+function HomepageHeader() {
+  React.useEffect(() => {
+    window.location.href = useBaseUrl('/docs/main');
+  }, []);
+  return null;
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Redirect to='/docs/getting_started'  />
+  );
+}
