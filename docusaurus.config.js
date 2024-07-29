339c594d9e0bf5399382fa8deef1eb8a05b2b279
Thearteus <arteuspw@proton.me>
2024-07-30 00:51:28 +0300

    Algolia Search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,29 +30,23 @@ const config = {
     locales: ["en"],
   },
 
-
-  
   presets: [
     [
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
+          routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/pole-engineering/Docs/tree/gh-pages/",
+          editUrl: "https://github.com/pole-engineering/Docs/tree/gh-pages/",
         },
         blog: {
-         
-
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/pole-engineering/Docs/tree/gh-pages/",
+          editUrl: "https://github.com/pole-engineering/Docs/tree/gh-pages/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -64,6 +58,38 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "LJ3565G2XN",
+
+        // Public API key: it is safe to commit it
+        apiKey: "7bd17f6cdc86234ae432961cf8c61ef5",
+
+        indexName: "Pole",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: "/docs/", // or as RegExp: /\/docs\//
+          to: "/",
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        insights: false,
+
+        //... other Algolia params
+      },
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
@@ -78,7 +104,7 @@ const config = {
             position: "left",
             label: "Getting Started",
           },
-    
+
           { to: "/configurator", label: "Configurator", position: "left" },
           { to: "/mods", label: "Mods", position: "left" },
           { to: "/gallery", label: "Gallery", position: "left" },
@@ -158,7 +184,6 @@ const config = {
         disableSwitch: true,
         respectPrefersColorScheme: false,
       },
-      
     }),
 };
 
